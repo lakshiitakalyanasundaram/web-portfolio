70945f3ef026ec6ea9486fbe5772f0273f93e52d
lakshiitakalyanasundaram <3004lakshu@gmail.com>
2025-03-02 13:12:56 +0530

    Initial commit for existing project

---
+++ src/constants/index.js
@@ -0,0 +1,222 @@
+export const navLinks = [
+    {
+        id: 1,
+        name: 'Home',
+        href: '#home',
+    },
+    {
+        id: 2,
+        name: 'About',
+        href: '#about',
+    },
+    {
+        id: 3,
+        name: 'Work',
+        href: '#work',
+    },
+    {
+        id: 4,
+        name: 'Contact',
+        href: '#contact',
+    },
+];
+
+export const clientReviews = [
+    {
+        id: 1,
+        name: 'Emily Johnson',
+        position: 'Marketing Director at GreenLeaf',
+        img: 'assets/review1.png',
+        review:
+            'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+    },
+    {
+        id: 2,
+        name: 'Mark Rogers',
+        position: 'Founder of TechGear Shop',
+        img: 'assets/review2.png',
+        review:
+            'Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+    },
+    {
+        id: 3,
+        name: 'John Dohsas',
+        position: 'Project Manager at UrbanTech ',
+        img: 'assets/review3.png',
+        review:
+            'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+    },
+    {
+        id: 4,
+        name: 'Ether Smith',
+        position: 'CEO of BrightStar Enterprises',
+        img: 'assets/review4.png',
+        review:
+            'Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
+    },
+];
+
+export const myProjects = [
+    {
+        title: 'Digital Lock and Key',
+        company: 'Atsuya Technologies Pvt Ltd',
+        date: 'June-July 2024',
+        desc: ' A secure digital lock system using STSAFE A110 and ESP32 , featuring **enhanced encryption, subscription-based keys, and controlled access management** for robust data protection.',
+        subdesc:
+            'Developed a secure digital lock system using STSAFE A110 and ESP32. Enhanced security and control over digital lock access management.\n' +
+            'Improved encryption and optimized key management for data integrity. Implemented encryption, subscription-based keys, and Arduino IDE integration. Ensured secure, controlled digital lock access with robust data protection.',
+        href: "public/Report.pdf",
+        texture: 'public/textures/project/giphy.mp4',
+        logo: '/assets/project-logo1.png',
+        logoStyle: {
+            backgroundColor: '#000000 ',
+            border: '0.2px solid #36201D',
+            boxShadow: '0px 0px 60px 0px #000000 ',
+        },
+        spotlight: '/assets/spotlight1.png',
+        tags: [
+            {
+                id: 1,
+                name: 'C',
+                path: '/assets/c-1.webp',
+            },
+            {
+                id: 2,
+                name: 'Arduino IDE',
+                path: '/assets/Arduino_Logo.webp',
+            },
+            {
+                id: 3,
+                name: 'ST Microelectronis',
+                path: '/assets/stmicroelectronics-ezgif.com-webp-to-png-converter.png',
+            },
+
+        ],
+    },
+
+    {
+        title: 'Comment Toxicity Detection',
+        company: 'Self-Initiated Project',
+        date: 'December 2024',
+        desc: 'An AI solution for real-time detection and classification of toxic comments, enhancing online safety and user experience by identifying harmful content using LLM techniques.',
+        subdesc:
+            'This project integrates advanced Natural Language Processing (NLP) techniques and AI models to classify comments as toxic or non-toxic, and categorize them based on toxicity type (e.g., insults, profanity, hate speech, etc.).\n' +
+            '\n' +
+            'With a user-friendly Streamlit interface, users can easily input comments and receive instant feedback on the toxicity and its type. It uses the Meta-Llama-3.1-8B-Instruct-Turbo model for precise analysis.\n' +
+            '\n',
+        href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
+        texture: 'public/textures/project/project2.mp4',
+        logo: 'public/assets/comment toxicitiy detection logo  copy 2.png',
+        logoStyle: {
+            backgroundColor: '#13202F',
+            border: '0.2px solid #17293E',
+            boxShadow: '0px 0px 60px 0px #2F6DB54D',
+        },
+        spotlight: '/assets/spotlight2.png',
+        tags: [
+            {
+                id: 1,
+                name: 'Python',
+                path: 'public/assets/PYTHON .png',
+            },
+            {
+                id: 2,
+                name: 'AI',
+                path: 'public/assets/AI .png',
+            },
+            {
+                id: 3,
+                name: 'StreamLit',
+                path: 'public/assets/streamlit .png',
+            },
+            {
+                id: 4,
+                name: 'oLamma',
+                path: 'public/assets/ollama.png',
+            },
+        ],
+    },
+
+
+    {
+        title: 'Custom Cattle Pet-Feeder',
+        company: 'Academic Project',
+        date: 'January-May 2024',
+        desc: 'Developed an IoT-based automatic pet feeder with real-time monitoring and adaptive\n' +
+            'feeding.',
+        subdesc:
+            'Revolutionized livestock management with precise data collection and real-time\n' +
+            'monitoring.\n' +
+            'Enhanced livestock health and sustainability through an efficient feeding system.\n' +
+            'Engineered the feeder using machine learning to adjust feeding patterns based on\n' +
+            'data analysis.\n' +
+            'Improved livestock health management, promoting sustainable farming and optimal\n' +
+            'pet nutrition.',
+        href: 'public/petfeeder.pdf',
+        texture: 'public/textures/project/petfeeder_QmgalOHL.mp4',
+        logo: 'public/assets/petfeeder.png',
+        logoStyle: {
+            backgroundColor: '#60f5a1',
+            background:
+                'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+            border: '0.2px solid rgba(208, 213, 221, 1)',
+            boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+        },
+        spotlight: '/assets/spotlight3.png',
+        tags: [
+            {
+                id: 1,
+                name: 'Arduino IDE',
+                path: 'public/assets/Arduino_Logo.webp',
+            },
+            {
+                id: 2,
+                name: 'C',
+                path: 'public/assets/c-1.webp',
+            },
+
+        ],
+    },
+
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+    return {
+        deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+        deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+        cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+        reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+        ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+        targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+    };
+};
+
+export const workExperiences = [
+    {
+        id: 1,
+        name: 'Framer',
+        pos: 'Lead Web Developer',
+        duration: '2022 - Present',
+        title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
+        icon: '/assets/framer.svg',
+        animation: 'victory',
+    },
+    {
+        id: 2,
+        name: 'Figma',
+        pos: 'Web Developer',
+        duration: '2020 - 2022',
+        title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
+        icon: '/assets/figma.svg',
+        animation: 'clapping',
+    },
+    {
+        id: 3,
+        name: 'Notion',
+        pos: 'Junior Web Developer',
+        duration: '2019 - 2020',
+        title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
+        icon: '/assets/notion.svg',
+        animation: 'salute',
+    },
+];
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,71 @@
+import {Suspense} from 'react';
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import HackerRoom from "../components/HackerRoom.jsx";
+import CanvasLoader from '../components/CanvasLoader';
+import { useMediaQuery } from "react-responsive";
+import {calculateSizes} from "../constants/index.js";
+import Target from "../components/Target.jsx";
+import ReactLogo from "../components/ReactLogo.jsx";
+import Cube from "../components/Cube";
+import Rings from "../components/Rings.jsx";
+import HeroCamera from "../components/HeroCamera.jsx";
+import Button from '../components/Button.jsx';
+
+const Hero = () => {
+    const isSmall = useMediaQuery({maxWidth: 440})
+    const isMobile= useMediaQuery({maxWidth: 768});
+    const isTablet= useMediaQuery({minWidth:768,maxWidth: 1024});
+
+    const sizes = calculateSizes(isSmall,isMobile,isTablet);
+
+
+    return (
+        <section className="min-h-screen w-full flex flex-col relative">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+                    Hi, I am Lakshiita <span className="waving-hand">👋🏼</span>
+                </p>
+                <p className="hero_tag text-gray-500">Crafting Code, Creating Impact</p>
+            </div>
+            <div className="w-full h-full absolute inset-0">
+                {/* <Leva/> */}
+                <Canvas className="w-full h-full">
+                    <Suspense fallback={<CanvasLoader/>}>
+                        <PerspectiveCamera makeDefault position={[0, 0, 30]}/>
+
+                        <HeroCamera isMobile={isMobile}>
+                            <HackerRoom
+                                position={sizes.deskPosition}
+                                scale={sizes.deskScale}
+                                rotation={[0.1, -Math.PI, 0]}
+
+                            />
+                            </HeroCamera>
+
+                        <group>
+                            <Target position={sizes.targetPosition} />
+                            <ReactLogo position={sizes.reactLogoPosition} />
+                            <Cube position={sizes.cubePosition} />
+                            <Rings position = {sizes.ringPosition} />
+                        </group>
+
+                        <ambientLight intensity={1}/>
+                        <directionalLight position={[10, 10, 10]} intensity={0.5}/>
+
+                    </Suspense>
+                </Canvas>
+            </div>
+
+            {/*<div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+                <a href="public/assets/K. Lakshiita (Resume).pdf" target="_blank" rel="noopener noreferrer" className="w-fit">
+                    <Button name="Click here for Resume" isBeam containerClass="sm:w-fit w-full sm:min-w-96"/>
+                </a>
+
+            </div>
+            */}
+        </section>
+    );
+};
+
+export default Hero;
